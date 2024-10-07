ff20cc3616317c945f8a1faff1bc5d0318aae07e
Jake <jacob7395@gmail.com>
2024-10-07 19:58:48 +0100

    feature: implemented serilog configuration for unity and file sync

--- src/HappyMods.Core/Config/SerilogConfiguration.cs
+++ src/HappyMods.Core/Config/SerilogConfiguration.cs
@@ -1,18 +1,59 @@
-﻿using System.IO;
-using System.Threading;
+﻿using System;
+using System.IO;
 using HappyMods.Core.Unity;
 using Serilog;
+using Serilog.Configuration;
 using Serilog.Core;
 using Serilog.Events;
+using UnityEngine;
+using Logger=Serilog.Core.Logger;
 
 namespace HappyMods.Core.Config;
 
-public class SerilogConfiguration(UnityConstants unityConstants)
+public static class SerilogConfiguration
 {
-    public void Init(string modPrefix)
+    public static Logger CreateLogger(UnityConstants unityConstants, string modPrefix)
     {
-        Log.Logger = new LoggerConfiguration()
-                     .WriteTo.File(Path.Combine($"{unityConstants.PersistentDataPath}", $"Happy.{modPrefix}.log"))
+        return new LoggerConfiguration()
+                     .WriteTo.File(Path.Combine($"{unityConstants.PersistentDataPath}", $"Happy.{modPrefix}.{DateTime.Now}.log"),
+                         outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                     .WriteTo.UnitySink(modPrefix)
                      .CreateLogger();
+    }
+}
+
+public class UnityLogSink(string modPrefix) : ILogEventSink
+{
+    private string AddPrefix(string logMessage) => $"[{modPrefix}] {logMessage}";
+    
+    public void Emit(LogEvent logEvent)
+    {
+        switch (logEvent.Level)
+        {
+            case LogEventLevel.Verbose:
+            case LogEventLevel.Debug:
+            case LogEventLevel.Information:
+                Debug.Log(AddPrefix(logEvent.RenderMessage()));
+                break;
+            case LogEventLevel.Warning:
+                Debug.LogWarning(AddPrefix(logEvent.RenderMessage()));
+                break;
+            case LogEventLevel.Error:
+            case LogEventLevel.Fatal:
+                Debug.LogError(AddPrefix(logEvent.RenderMessage()));
+                break;
+        }
+
+        if (logEvent.Exception is {} exception)
+            Debug.LogException(exception);
+    }
+}
+
+public static class UnityLogSinkExtensions
+{
+    public static LoggerConfiguration UnitySink(this LoggerSinkConfiguration loggerConfiguration,
+                                                string modPrefix)
+    {
+        return loggerConfiguration.Sink(new UnityLogSink(modPrefix), LogEventLevel.Warning);
     }
 }
